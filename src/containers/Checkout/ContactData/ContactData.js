4dfefc19654a3fd5603f70e472597234f4ff5883
jimmy2952 <jimmy2952@gmail.com>
2020-09-24 08:53:36 +0800

    improvement

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import axios from '../../../axios-orders'
 import { connect } from 'react-redux'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import { checkValidity } from '../../../shared/utility'
 
 class ContactData extends Component {
     state = {
@@ -113,33 +114,11 @@ class ContactData extends Component {
         
     }
 
-    checkValidity = (value, rules) => {
-        let isValid = true
-
-        if (!rules) {
-            return true
-        }
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        return isValid
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = JSON.parse(JSON.stringify(this.state.orderForm))
 
         updatedOrderForm[inputIdentifier].value = event.target.value
-        updatedOrderForm[inputIdentifier].valid = this.checkValidity(updatedOrderForm[inputIdentifier].value, updatedOrderForm[inputIdentifier].validation)
+        updatedOrderForm[inputIdentifier].valid = checkValidity(updatedOrderForm[inputIdentifier].value, updatedOrderForm[inputIdentifier].validation)
         updatedOrderForm[inputIdentifier].touched = true
         console.log(this.state.orderForm)
 
